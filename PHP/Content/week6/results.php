84c7ea30e34de8bfe35cd88839ad9d7d476285be
davitpra <davitpra@Davit>
2024-02-06 20:40:25 -0500

    PHP week6

---
+++ PHP/Content/week6/results.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mad Libs Results</title>
+</head>
+<body>
+    <?php
+        echo "<p>It was one night everybody dreads -  the night before finals week. The library was full of (adjective) students all glued to their books and (body part) deep in enery drinks and coffee. One desperate student even had the guts to sneak in a(n) (object) into the library! Finally, around 5am, I grabbed my (object), left the library and headed back to my dorn room. Unfortunately for me, it was so late that I was attacked by a not so friendly (animal) who ate all my study notes.</p>"  
+    ?>
+</body>
+</html>
